9826b8c210134b60425e00179d1fc83aac42b66c
Adriano Miguel <101981502+AdrianoMiguell@users.noreply.github.com>
2022-10-17 17:01:24 -0300

    version4

--- site/resources/views/layouts/navigation.blade.php
+++ site/resources/views/layouts/navigation.blade.php
@@ -1,5 +1,11 @@
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg bg-light shadow-sm">
     <div class="container-fluid">
         <div class="row col-2"> @include('components.application-logo') </div>
+        <div class="d-flex justify-content-end">
+            <a href="#" class="dropdown-toggle btn btn-outline-success rounded" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {{Auth::user()->name}}
+            </a>
+            
+        </div>
     </div>
 </nav>
